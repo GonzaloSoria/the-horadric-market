ffe3489d64edd419a4ed9929b00708e6c99ff841
GonzaloSoria <gonzalo.sooria@gmail.com>
2023-05-05 18:19:26 -0300

    changed styles

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,14 +7,21 @@ export const Hero = () => {
     <>
         <div className="hero-container py-40">
             <div className="container mx-auto py-52">
-                <div className="grid grid-cols-2 gap-4">
+                {/* <div className="grid grid-cols-2 gap-4">
                     <div>
+                        <img src={diabloIVLogo} alt="Diablo IV logo" />
+
                         <h1 className='text-first text-6xl leading-tight font-bold font-roboto'>Potencia tu personaje en Diablo IV intercambiando objetos con otros jugadores</h1>
                     </div>
                     <div className='mx-auto'>
                         <img src={diabloIVLogo} alt="Diablo IV logo" />
                     </div>
+                </div> */}
+                <div className="container mx-auto text-center">
+                    <img className='mx-auto' src={diabloIVLogo} alt="Diablo IV logo"/>
+                    <h1 className='text-first text-6xl leading-tight font-bold font-cinzel'>Potencia tu personaje intercambiando objetos con otros jugadores</h1>
                 </div>
+                
             </div>
         </div>
         <img src={divider} alt="" />
--- src/components/HowWorks/index.jsx
+++ src/components/HowWorks/index.jsx
@@ -6,8 +6,16 @@ export const HowWorks = () => {
     <>
         <div className="how-works-container py-28">
             <div className="container mx-auto">
-                <h2 className='font-cinzel text-first text-5xl font-bold text-center'>¿Cómo funciona The Horadric Market?</h2>
-                <p className='font-roboto text-first text-2xl text-center py-12'>En esta plataforma podrás publicar los objetos que tienes en tu inventario y ver los objetos que otros jugadores están ofreciendo para intercambiar. ¡Encuentra el objeto perfecto para tu personaje!</p>
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <h2 className='font-cinzel text-first text-5xl font-bold text-center'>¿Cómo funciona The Horadric Market?</h2>
+                </div>
+                <div>
+                  <p className='font-roboto text-first text-2xl'>En esta plataforma podrás publicar los objetos que tienes en tu inventario y ver los objetos que otros jugadores están ofreciendo para intercambiar. ¡Encuentra el objeto perfecto para tu personaje!</p>
+                </div>
+              </div>
+                {/* <h2 className='font-cinzel text-first text-5xl font-bold text-center'>¿Cómo funciona The Horadric Market?</h2>
+                <p className='font-roboto text-first text-2xl text-center py-12'>En esta plataforma podrás publicar los objetos que tienes en tu inventario y ver los objetos que otros jugadores están ofreciendo para intercambiar. ¡Encuentra el objeto perfecto para tu personaje!</p> */}
             </div>
         </div>
         <img src={divider} alt="" />
